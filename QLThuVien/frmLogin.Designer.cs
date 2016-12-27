3925e7592eb21e28b88f0ceb32c0dfe2be58ea18
minhloc369 <minhloc369@hmail.com>
2016-12-27 13:07:55 +0700

    add frmMenu and frmLogin

---
+++ QLThuVien/frmLogin.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace QLThuvien
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAcc = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.ckbShowPass = new System.Windows.Forms.CheckBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.lbIncorrect = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(48, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Account";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(48, 126);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Password";
+            // 
+            // txtAcc
+            // 
+            this.txtAcc.Location = new System.Drawing.Point(150, 65);
+            this.txtAcc.Name = "txtAcc";
+            this.txtAcc.Size = new System.Drawing.Size(212, 22);
+            this.txtAcc.TabIndex = 0;
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(150, 123);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(212, 22);
+            this.txtPass.TabIndex = 1;
+            this.txtPass.UseSystemPasswordChar = true;
+            this.txtPass.TextChanged += new System.EventHandler(this.txtPass_TextChanged);
+            this.txtPass.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtPass_KeyDown);
+            // 
+            // ckbShowPass
+            // 
+            this.ckbShowPass.AutoSize = true;
+            this.ckbShowPass.Location = new System.Drawing.Point(73, 180);
+            this.ckbShowPass.Name = "ckbShowPass";
+            this.ckbShowPass.Size = new System.Drawing.Size(129, 21);
+            this.ckbShowPass.TabIndex = 2;
+            this.ckbShowPass.Text = "Show Password";
+            this.ckbShowPass.UseVisualStyleBackColor = true;
+            this.ckbShowPass.CheckedChanged += new System.EventHandler(this.ckbShowPass_CheckedChanged);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(216, 267);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(85, 31);
+            this.btnLogin.TabIndex = 3;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // lbIncorrect
+            // 
+            this.lbIncorrect.ForeColor = System.Drawing.Color.Red;
+            this.lbIncorrect.Location = new System.Drawing.Point(92, 216);
+            this.lbIncorrect.Name = "lbIncorrect";
+            this.lbIncorrect.Size = new System.Drawing.Size(259, 29);
+            this.lbIncorrect.TabIndex = 5;
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(606, 412);
+            this.Controls.Add(this.lbIncorrect);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.ckbShowPass);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtAcc);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Name = "frmLogin";
+            this.Text = "frmLogin";
+            this.Load += new System.EventHandler(this.frmLogin_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAcc;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.CheckBox ckbShowPass;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label lbIncorrect;
+    }
+}
---
+++ QLThuVien/frmLogin.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace QLThuvien
+{
+    public partial class frmLogin : Form
+    {
+        SqlConnection cn;
+        SqlCommand cmd;
+        Database db = new Database();
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+            string cnStr = "Server = .; Database = qlthuvien; Integrated Security = true;";
+            cn = new SqlConnection(cnStr);
+        }
+        private void connect()
+        {
+            {
+                try
+                {
+                    if (cn != null && cn.State == ConnectionState.Closed)
+                        cn.Open();
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+        private void disconnect()
+        {
+            if (cn != null && cn.State == ConnectionState.Open)
+                cn.Close();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            connect();
+            try
+            {
+                string sql = "select count(*) from [QLThuvien].[dbo].[User] where ID = @id and Password = @pass";
+                cmd = new SqlCommand(sql, cn);
+                cmd.Parameters.Add(new SqlParameter("@id", txtAcc.Text));
+                cmd.Parameters.Add(new SqlParameter("@pass", txtPass.Text));
+                int x = (int)cmd.ExecuteScalar();
+                if (x == 1)
+                {//Đăng nhập thành công
+                    MessageBox.Show("Đăng nhập thành công", "Thông báo");
+                    this.Hide();
+                    frmMenu frm = new frmMenu();
+                    frm.ShowDialog();
+                }
+                else
+                {
+                    lbIncorrect.Text = "ID hoặc password không đúng !";
+                    txtAcc.Text = "";
+                    txtPass.Text = "";
+                }
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                disconnect();
+            }
+        }
+
+        private void ckbShowPass_CheckedChanged(object sender, EventArgs e)
+        {
+            if (ckbShowPass.Checked)
+            {
+                txtPass.UseSystemPasswordChar = false;
+            }
+            else
+            {
+                txtPass.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void txtPass_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void txtPass_KeyDown(object sender, KeyEventArgs e)
+        {
+            object dummy = null;
+            EventArgs dummye = null;
+            if (e.KeyCode == Keys.Enter || e.KeyCode == Keys.Return)
+            {
+                btnLogin_Click(dummy, dummye);
+            }
+
+        }
+    }
+}
